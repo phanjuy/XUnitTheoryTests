3a32d8cf0c8a0286f5e9ac74e0460a4ce885053e
Duy Phan <dpha@netcompany.com>
2022-01-01 16:03:57 +0100

    Basic tests using xUnit [Fact]

---
+++ XUnitTheoryTests/CalculatorTests.cs
@@ -0,0 +1,20 @@
+﻿using Xunit;
+
+namespace XUnitTheoryTests
+{
+    public class CalculatorTests
+    {
+        [Fact]
+        public void CanAdd()
+        {
+            var calculator = new Calculator();
+
+            int value1 = 1;
+            int value2 = 2;
+
+            var result = calculator.Add(value1, value2);
+
+            Assert.Equal(3, result);
+        }
+    }
+}
