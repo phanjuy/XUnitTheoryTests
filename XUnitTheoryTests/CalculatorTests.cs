04822d3652b2aac4670769d73cbf82d6a6cdba71
Duy Phan <dpha@netcompany.com>
2022-01-01 16:36:20 +0100

    Load data from a method on the test class with [MemberData]

--- XUnitTheoryTests/CalculatorTests.cs
+++ XUnitTheoryTests/CalculatorTests.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using Xunit;
 
 namespace XUnitTheoryTests
@@ -52,6 +53,30 @@ namespace XUnitTheoryTests
             var result = calculator.Add(value1, value2);
 
             Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [MemberData(nameof(GetData), parameters: 3)]
+        public void CanAddTheoryMemberDataMethod(int value1, int value2, int expected)
+        {
+            var calculator = new Calculator();
+
+            var result = calculator.Add(value1, value2);
+
+            Assert.Equal(expected, result);
+        }
+
+        public static IEnumerable<object[]> GetData(int numTests)
+        {
+            var allData = new List<object[]>
+            {
+                new object[] { 1, 2, 3 },
+                new object[] { -4, -6, -10 },
+                new object[] { -2, 2, 0 },
+                new object[] { int.MinValue, -1, int.MaxValue },
+            };
+
+            return allData.Take(numTests);
         }
 
         public static IEnumerable<object[]> Data =>
