84cf27eb765a8ceadb35078de1329107b463c0bc
Duy Phan <dpha@netcompany.com>
2022-01-01 16:10:10 +0100

    Parameterised tests using [Theory] and [InlineData]
    Using the [Theory] attribute to create parameterised tests with [InlineData]

--- XUnitTheoryTests/CalculatorTests.cs
+++ XUnitTheoryTests/CalculatorTests.cs
@@ -16,5 +16,19 @@ namespace XUnitTheoryTests
 
             Assert.Equal(3, result);
         }
+
+        [Theory]
+        [InlineData(1, 2, 3)]
+        [InlineData(-4, -6, -10)]
+        [InlineData(-2, 2, 0)]
+        [InlineData(int.MinValue, -1, int.MaxValue)]
+        public void CanAddTheory(int value1, int value2, int expected)
+        {
+            var calculator = new Calculator();
+
+            var result = calculator.Add(value1, value2);
+
+            Assert.Equal(expected, result);
+        }
     }
 }
